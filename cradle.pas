0c1100466299476c67749755ef7ff25eef27a8af
TL Jung <donghee.jung@gmail.com>
2019-04-30 05:32:27 +0900

    소스 정비 및 불용 소스 삭제

--- cradle.pas
+++ cradle.pas
@@ -3,129 +3,107 @@ program Cradle;
 {-----------------------------------------------------}
 
 { Constant Declarations }
-const TAB = ^I;
+const 
+TAB = ^I; 
+{ CR = ^M; }      { DOS & Windows }
+CR = ^J;          { Linux & BSD }
 {-----------------------------------------------------}
 
-{Variable Declarations }
-var Look: char;
-{-----------------------------------------------------}
+{ Variable Declarations }
+var
+Look: char;       { Lookahead Character }
+{--------------------------------------------------------------}
 
+{ Read New Character From Input Stream }
 procedure GetChar;
 begin
-  Read(Look);
+   Read(Look);
 end;
-{-----------------------------------------------------}
-
+{--------------------------------------------------------------}
+{ Report an Error }
 procedure Error(s: string);
-begin { Error }
+begin
    WriteLn;
    WriteLn(^G, 'Error: ', s, '.');
-end; { Error }
-{-----------------------------------------------------}
-
+end;
+{--------------------------------------------------------------}
+{ Report Error and Halt }
 procedure Abort(s: string);
-begin { Abort }
+begin
    Error(s);
    Halt;
-end; { Abort }
-{-----------------------------------------------------}
-
-procedure Expected(s: string);
-begin 
-	Abort(s + ' Expected');
 end;
-{-----------------------------------------------------}
-
+{--------------------------------------------------------------}
+{ Report What Was Expected }
+procedure Expected(s: string);
+begin
+   Abort(s + ' Expected');
+end;
+{--------------------------------------------------------------}
+{ Match a Specific Input Character }
 procedure Match(x: char);
 begin
-	if Look = x then GetChar
-	else Expected('''' + x + '''');
+   if Look = x then GetChar
+   else Expected('''' + x + '''');
 end;
-{-----------------------------------------------------}
-	
+{--------------------------------------------------------------}
+{ Recognize an Alpha Character }
 function IsAlpha(c: char): boolean;
 begin
-	IsAlpha := upcase(c) in ['A'..'Z'];
+   IsAlpha := upcase(c) in ['A'..'Z'];
 end;
-{-----------------------------------------------------} 
-
+{--------------------------------------------------------------}
+{ Recognize a Decimal Digit }
 function IsDigit(c: char): boolean;
 begin
-	IsDigit := c in ['0'..'9'];
+   IsDigit := c in ['0'..'9'];
 end;
-{-----------------------------------------------------} 
-
+{--------------------------------------------------------------}
+{ Get an Identifier }
 function GetName: char;
 begin
-	if not IsAlpha(Look) then Expected('Name');
-	GetName := UpCase(Look);
-	GetChar;
-end;
-{-----------------------------------------------------}
-
-function GetNum: char;
-begin
-   if not IsDigit(Look) then Expected('Integer');
-   GetNum := Look;
+   if not IsAlpha(Look) then Expected('Name');
+   GetName := UpCase(Look);
    GetChar;
 end;
-{-----------------------------------------------------}
-
+{--------------------------------------------------------------}
+{ Get a Number }
+function GetNum: integer;
+begin
+   if not IsDigit(Look) then Expected('Integer');
+   GetNum := Ord(Look) - Ord('0');
+   GetChar;
+end;
+{--------------------------------------------------------------}
+{ Output a String with Tab }
 procedure Emit(s: string);
 begin
-	Write(TAB, s);
+   Write(TAB, s);
 end;
-{-----------------------------------------------------} 
-
+{--------------------------------------------------------------}
+{ Output a String with Tab and CRLF }
 procedure EmitLn(s: string);
 begin
-	Write(TAB, s);
-	WriteLn;
+   Emit(s);
+   WriteLn;
 end;
-{-----------------------------------------------------}
-
-procedure Term;
-begin
-   EmitLn('MOVE #' + GetNum + ',D0')
-end;
-{-----------------------------------------------------}
-
-procedure Add;
-begin
-   Match('+');
-   Term;
-   EmitLn('ADD D1,D0');
-end;
-{-----------------------------------------------------}
-
-procedure Subtract;
-begin
-   Match('-');
-   Term;
-   EmitLn('SUB D1,D0');
-end;
-{-----------------------------------------------------}
-
-procedure Expression;
-begin
-   Term;
-   EmitLn('MOVE D0,D1');
-   case Look of
-     '+' : Add;
-     '-' : Subtract;
-   else Expected('Addop');
-   end;
-end;
-{-----------------------------------------------------}
-
+{--------------------------------------------------------------}
+{ Initialize }
 procedure Init;
 begin
-	GetChar;
+   GetChar;
 end;
+{--------------------------------------------------------------}
+{ Parse and Translate an Expression }
+function Expression: integer;
+begin
+   Expression := GetNum;
+end;
+{--------------------------------------------------------------}
 
 { Main Program }
 begin
    Init;
-   Expression;
+   WriteLn(Expression);
 end.
-{-----------------------------------------------------} 
+{--------------------------------------------------------------}
--- cradle.py
+++ cradle.py
@@ -4,9 +4,10 @@
 # cradle.py
 def read_char(prompt):
     str_buffer = input(prompt)
+    str_buffer += '\n'
     str_len = len(str_buffer)
     idx = 0
-    
+
     while True:
         while idx < str_len:
             yield str_buffer[idx]
@@ -14,28 +15,27 @@ def read_char(prompt):
         str_buffer = input(prompt)
         str_len = len(str_buffer)
         idx = 0
-    print("** generator ends **")
 
-    
+
 def windowsBell():
     winsound.Beep(500, 300)
 
-    
+
 def macosBell():
     print('\a')
 
-    
+
 def linuxBell():
     print('\a')
-    
+
 
 def GetChar():
-    global Look    
+    global Look
     Look = next(char_gen)
-    
+
 
 def Error(string):
-    print
+    print()
     EmitBell()
     print('Error: ' + string + '.')
 
@@ -44,64 +44,163 @@ def Abort(string):
     Error(string)
     quit()
 
-    
+
 def Expected(string):
     Abort(string + ' Expected')
+
+
+def IsAddop(ch):
+    ret_val = ch in {'+', '-'}
+    return ret_val
+
+
+def skip_white():
+    # python isspace() includes '\n' & fail
+    while Look == '\t' or Look == ' ':
+        GetChar()
+
+
+def GetNum():
+    value = ''
+    if not Look.isnumeric():
+        Expected('Integer')
+    
+    while Look.isnumeric():
+        value += Look
+        GetChar()
+        
+    ret_val = value
+    skip_white()
+    return ret_val
+
+
+def GetName():
+    token = ''
+    if not Look.isalpha():
+        Expected('Name')
+
+    while (Look.isalpha() or Look.isnumeric()):
+        token += Look
+        GetChar()
+
+    ret_val = token
+    skip_white()
+    return ret_val.upper()
+
+
+def Emit(string):
+    print('\t' + string, end='')
+
+
+def EmitLn(string):
+    print('\t' + string)
+
 
 def Match(ch):
     if Look == ch:
         GetChar()
+        skip_white()
     else:
         Expected('\'' + ch + '\'')
-        
-def GetNum():
-    if not Look.isnumeric():
-        Expected('Integer')
-    ret_val = Look
-    GetChar()
-    return ret_val
-    
-def GetName():
-    if not Look.isalpha():
-        Expected('Name')
-    ret_val = Look
-    GetChar()
-    return ret_val.upper()
-    
-def Emit(string):
-    print('\t' + string, end = '')
 
-def EmitLn(string):
-    print('\t' + string)
-    
+
+def ident():
+    name = ''
+    name = GetName()
+    if Look == '(':
+        Match('(')
+        Match(')')
+        EmitLn('BSR ' + name)
+    else:
+        EmitLn('MOVE ' + name + '(PC),D0')
+
+
+def Multiply():
+    Match('*')
+    Factor()
+    EmitLn('MULS (SP)+,D0')
+
+
+def Divide():
+    Match('/')
+    Factor()
+    EmitLn('MOVE (SP)+,D1')
+    EmitLn('DIVS D0,D1')
+    EmitLn('MOVE D1,D0')
+
+
 def Add():
     Match('+')
     Term()
-    EmitLn('ADD D1,D0')
-    
+    EmitLn('ADD (SP)+,D0')
+
+
 def Subtract():
     Match('-')
     Term()
-    EmitLn('SUB D1,D0')
-    
+    EmitLn('SUB (SP)+,D0')
+    EmitLn('NEG D0')
+
+
 def start():
     GetChar()
+    skip_white()
+
+
+def Factor():
+    if Look == '(':
+        Match('(')
+        expression()
+        Match(')')
+    elif Look.isalpha():
+        ident()
+    else:
+        EmitLn('MOVE #' + GetNum() + ',D0')
+
 
 def Term():
-    EmitLn('MOVE #' + GetNum() + ',D0')
+    Factor()
+    while Look in {'*', '/'}:
+        EmitLn('MOVE D0,-(SP)')
+        if Look == '*':
+            Multiply()
+        elif Look == '/':
+            Divide()
+        else:
+            Expected('Mulop')   
+
 
 def expression():
-    Term()
-    EmitLn('MOVE D0,D1')
-    if Look == '+':
-        Add()
-    elif Look == '-':
-        Subtract()
+    if IsAddop(Look):
+        EmitLn('CLR D0')
+    else:
+        Term()
+    while Look in {'+', '-'}:
+        EmitLn('MOVE D0,-(SP)')
+        if Look == '+':
+            Add()
+        elif Look == '-':
+            Subtract()
+        else:
+            Expected('Addop')
+
+
+def assignment():
+    name = ''
+    name = GetName()
+    Match('=')
+    expression()
+    EmitLn('LEA ' + name + '(PC),A0')
+    EmitLn('MOVE D0,(A0)')
+
+
 
 def main():
-
     start()
-    expression()
+    assignment()
+    if Look != '\n':
+        Expected('Newline')
+    
     return 0
 
 
@@ -112,7 +211,7 @@ char_gen = read_char("Enter any string: ")
 if __name__ == "__main__":
     # Try reading several files varying the buffer size
     import sys
-    
+
     os = sys.platform
     if os == 'win32':
         import winsound
@@ -121,21 +220,6 @@ if __name__ == "__main__":
         EmitBell = linuxBell
     elif os == 'darwin':
         EmitBell = macosBell
-    
-    if len(sys.argv) == 1:
-        pass
-    elif len(sys.argv) == 2:
-        first_arg = int(sys.argv[1])
-        print('First arg = ' + first_arg)
-    else:
-        first_arg = sys.argv[1]
-        print('First arg = ' + first_arg)
-        other_args  = sys.argv[2:]
-        print('Other args = ')
-        for x in other_args:
-            print(x)
-    
-    print('Here comes the main routine')
+
     ret_val = main()
-    
     sys.exit(ret_val)
